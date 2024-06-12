5dad2267de9f6e2768230aa08223dee27dd806b3
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-06-12 23:01:59 +0530

    Time: 4 ms (95.22%), Space: 44.7 MB (47.99%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -21,10 +21,16 @@ class MinStack {
     }
     
     public int top() {
+        if(stack.isEmpty()){
+            return -1;
+        }
         return stack.peek();
     }
     
     public int getMin() {
+        if(minStack.isEmpty()){
+            return -1;
+        }
         return minStack.peek();
     }
 }
