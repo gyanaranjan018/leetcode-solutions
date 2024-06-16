95aab338d514d5547976f7c6f288bfe2c55c6012
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-06-17 02:25:07 +0530

    Time: 8 ms (32.38%), Space: 45.3 MB (74.44%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.java
+++ 0800-letter-case-permutation/0800-letter-case-permutation.java
@@ -7,22 +7,24 @@ class Solution {
     }
 
     public void helper(String s, String op, List<String> res){
-        while(s.length() > 0 && s.charAt(0)>= 48 && s.charAt(0) <= 57){
-            op += s.charAt(0);
-            s = s.substring(1);
-        }
         if(s.length() == 0){
             res.add(op);
             return;
         }
+        char c = s.charAt(0);
+        if(Character.isDigit(c)){
+            op += c;
+            helper(s.substring(1), op, res);
+        }
+        else{
+            String op1 = op;
+            String op2 = op;
 
-        String op1 = op;
-        String op2 = op;
+            op1 += Character.toLowerCase(c);
+            op2 += Character.toUpperCase(c);
 
-        op1 += Character.toLowerCase(s.charAt(0));
-        op2 += Character.toUpperCase(s.charAt(0));
-
-        helper(s.substring(1), op1, res);
-        helper(s.substring(1), op2, res);
+            helper(s.substring(1), op1, res);
+            helper(s.substring(1), op2, res);
+        }
     }
 }
