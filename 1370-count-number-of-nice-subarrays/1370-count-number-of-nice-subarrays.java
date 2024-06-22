d01084e33c5a50106cb0c780cdbe288ae8fa3ac3
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-06-22 16:45:32 +0530

    Time: 3 ms (100%), Space: 51.3 MB (99.38%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -5,9 +5,7 @@ class Solution {
         int[] odd = new int[n+1];
         odd[0] = 1;
         for(int i = 0; i< n; i++){
-            if(nums[i] % 2 == 1){
-                oddCount++;
-            }            
+            oddCount += (nums[i] % 2);            
             if(oddCount-k >= 0){
                 result += odd[oddCount-k];
             }
