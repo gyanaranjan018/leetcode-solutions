00b54ff7ad4167a0f9d5347f4cd0ec462ab5282d
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-06-30 23:48:41 +0530

    Time: 2 ms (85.96%), Space: 45.2 MB (65.82%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,26 +2,30 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
+
         boolean[][] vis = new boolean[n][m];
 
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
+
         for(int row = 0; row < n; row++){
-            if(board[row][0] == 'O'){
-                dfs(row, 0, vis, board);
+            //first column
+            if(!vis[row][0] && board[row][0] == 'O'){ 
+                dfs(row, 0, vis, board, delRow, delCol);
             }
-        }
-        for(int row = 0; row < n; row++){
-            if(board[row][m-1] == 'O'){
-                dfs(row, m-1, vis, board);
+            //last column
+            if(!vis[row][m-1] && board[row][m-1] == 'O'){
+                dfs(row, m-1, vis, board, delRow, delCol);
             }
         }
         for(int col = 0; col < m; col++){
-            if(board[0][col] == 'O'){
-                dfs(0, col, vis, board);
+            //first row
+            if(!vis[0][col] && board[0][col] == 'O'){
+                dfs(0, col, vis, board, delRow, delCol);
             }
-        }
-        for(int col = 0; col < m; col++){
-            if(board[n-1][col] == 'O'){
-                dfs(n-1, col, vis, board);
+            //last row
+            if(!vis[n-1][col] && board[n-1][col] == 'O'){
+                dfs(n-1, col, vis, board, delRow, delCol);
             }
         }
 
@@ -34,21 +38,20 @@ class Solution {
         }
     }
 
-    public void dfs(int row, int col, boolean[][] vis, char[][] board){
+    //dfs traversal
+    public void dfs(int row, int col, boolean[][] vis, char[][] board, int[] delRow, int[] delCol){
         vis[row][col] = true;
 
         int n = board.length;
         int m = board[0].length;
 
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
-
+        //check for top, right, buttom, left
         for(int i = 0; i< 4; i++){
             int nrow = row + delRow[i];
             int ncol = col + delCol[i];
 
             if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && board[nrow][ncol] == 'O' && !vis[nrow][ncol]){
-                dfs(nrow, ncol, vis, board);
+                dfs(nrow, ncol, vis, board, delRow, delCol);
             }
         }
         return;
