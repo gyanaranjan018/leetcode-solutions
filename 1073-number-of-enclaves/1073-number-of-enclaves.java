c98ea91936a2c60a403e7140ce9493c7be152806
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-07-02 01:02:35 +0530

    Time: 9 ms (58.68%), Space: 54.2 MB (75.8%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -3,30 +3,51 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        int[] delRow = {1, 0, -1, 0};
-        int[] delCol = {0, 1, 0, -1};
-
         boolean[][] vis = new boolean[n][m];
+        Queue<Pair> q = new LinkedList<>();
 
         for(int row = 0; row < n; row++){
             if(!vis[row][0] && grid[row][0] == 1){
-                dfs(row, 0, vis, grid, n, m, delRow, delCol);
+                q.add(new Pair(row, 0));
+                vis[row][0] = true;
             }
             if(!vis[row][m-1] && grid[row][m-1] == 1){
-                dfs(row, m-1, vis, grid, n, m, delRow, delCol);
+                q.add(new Pair(row, m-1));
+                vis[row][m-1] = true;
             }
         }
 
         for(int col = 0; col < m; col++){
             if(!vis[0][col] && grid[0][col] == 1){
-                dfs(0, col, vis, grid, n, m, delRow, delCol);
+                q.add(new Pair(0, col));
+                vis[0][col] = true;
             }
             if(!vis[n-1][col] && grid[n-1][col] == 1){
-                dfs(n-1, col, vis, grid, n, m, delRow, delCol);
+                q.add(new Pair(n-1, col));
+                vis[n-1][col] = true;
             }
         }
 
-        int count =0;
+        int[] delRow = {1, 0, -1, 0};
+        int[] delCol = {0, 1, 0, -1};
+
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.remove();
+
+            for(int i =0; i< 4; i++){
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    q.add(new Pair(nrow, ncol));
+                    vis[nrow][ncol] = true;
+                }
+            }   
+        }
+
+        int count = 0;
 
         for(int i =0; i< n; i++){
             for(int j = 0; j< m; j++){
@@ -38,18 +59,12 @@ class Solution {
         return count;
     }
 
-    private void dfs(int row, int col, boolean[][] vis, int[][] grid, int n, int m, int[] delRow, int[] delCol){
-        vis[row][col] = true;
-
-        for(int i =0; i< 4; i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                dfs(nrow, ncol, vis, grid, n, m, delRow, delCol);
-            }
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row = row;
+            this.col = col;
         }
-
-        return;
     }
 }
