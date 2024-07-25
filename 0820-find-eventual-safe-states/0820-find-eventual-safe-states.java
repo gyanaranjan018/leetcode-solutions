1ac217e862600f2b3f1e152751a4289da66e1ced
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-07-26 01:34:03 +0530

    Time: 24 ms (45.82%), Space: 55.3 MB (51.22%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,44 +1,43 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int v = graph.length;
-        int[] vis = new int[v];
-        int[] pathVis = new int[v];
-        int[] safeNodes = new int[v];
-        List<Integer> res = new ArrayList<>();
+        List<List<Integer>> adjRev = new ArrayList<>();
 
-        for(int i =0; i< v; i++){
-            if(vis[i] == 0){
-                check(i, graph, vis, pathVis, safeNodes);
-            }
+        for(int i = 0; i< v; i++){
+            adjRev.add(new ArrayList<>());
         }
+
+        int indegree[] = new int[v];
 
         for(int i = 0; i < v; i++){
-            if(safeNodes[i] == 1){
-                res.add(i);
+            //i -> it
+            //it -> i
+            for(int it : graph[i]){
+                adjRev.get(it).add(i);
+                indegree[i]++;
             }
         }
-        return res;
-    }
 
-    private boolean check(int node, int[][] graph, int[] vis, int[] pathVis, int[] safeNodes){
-        vis[node] = 1;
-        pathVis[node] = 1;
-        safeNodes[node] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> safeNodes = new ArrayList<>();
+        for(int i = 0; i < v; i++){
+            if(indegree[i] ==0){
+                q.add(i);
+            }
+        }
 
-        for(int it : graph[node]){
-            //when node is not visited
-            if(vis[it] == 0){
-                if(check(it, graph, vis, pathVis, safeNodes) == true){
-                    return true;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            safeNodes.add(node);
+            for(int it : adjRev.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
                 }
             }
-            //if the node has been previously visited but it has to be visited on the same path 
-            else if(pathVis[it] == 1){ 
-                return true;
-            }
         }
-        safeNodes[node] = 1;
-        pathVis[node] = 0;
-        return false;
+
+        Collections.sort(safeNodes);
+        return safeNodes;
     }
 }
