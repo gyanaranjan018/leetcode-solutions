3a3803483f82ba7eb672783f94acb9e725ed66a9
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-07-30 23:05:56 +0530

    Time: 44 ms (25.07%), Space: 45.3 MB (57.66%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,26 +1,18 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int[] leftB = new int[n];
-        int[] rightA = new int[n];
         int bCount = 0;
-        for(int i = 0; i < n; i++){
-	        leftB[i] = bCount;
-	        if(s.charAt(i) == 'b'){
-		        bCount++;
-	        }
-        }
-
         int aCount = 0;
+        int res = Integer.MAX_VALUE;
         for(int i = n-1; i >= 0; i--){
-	        rightA[i] = aCount;
 	        if(s.charAt(i) == 'a'){
 		        aCount++;
 	        }
         }
-        int res = Integer.MAX_VALUE;
         for(int i = 0; i < n; i++){
-	        res = Math.min(leftB[i]+rightA[i], res);
+	        if(s.charAt(i) == 'a') aCount--;
+	            res = Math.min(bCount + aCount, res);
+	        if(s.charAt(i) == 'b') bCount++;
         }
         return res == Integer.MAX_VALUE ? 0 : res;
     }
