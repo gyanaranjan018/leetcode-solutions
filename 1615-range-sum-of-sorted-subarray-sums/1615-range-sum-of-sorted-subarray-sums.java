b8d3c1d0d472c19cf7c6e91539efa7a8d19360cb
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-08-04 23:37:27 +0530

    Time: 209 ms (10.81%), Space: 44.9 MB (82.43%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -7,27 +7,24 @@ class Solution {
         }
     }
     public int rangeSum(int[] nums, int n, int left, int right) {
-        long resArr[] = new long[(n*(n+1))/2];
         int c = 0;
         long mod = 1000000007;
         PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.sum - y.sum);
         for(int i =0; i< n; i++){
             pq.add(new Pair(nums[i], i));
         }
-
+        long res = 0;
         while(pq.size() != 0){
             int sum = pq.peek().sum;
             int idx = pq.peek().idx;
             pq.remove();
-            resArr[c++] = sum;
-
+            c++;
+            if(c >= left && c <= right){
+                res += sum;
+            }
             if(idx+1 < n){
                 pq.add(new Pair(sum + nums[idx+1], idx+1));
             }
-        }
-        long res = 0;
-        for(int i = left-1; i< right; i++){
-            res += resArr[i];
         }
         return (int)(res % mod);
     }
