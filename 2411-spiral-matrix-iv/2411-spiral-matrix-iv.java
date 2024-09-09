1e8a047ad5a4d7ebe2811ddf56e0628f3174e9a3
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-09-10 01:22:17 +0530

    Time: 6 ms (97.47%), Space: 56.5 MB (81.68%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
@@ -19,28 +19,28 @@ class Solution {
         int top = 0, down = m-1, left = 0, right = n-1;
         int id = 0;
         while(top <= down && left <= right){
-            if(id == 0){
+            if(id == 0){ //left to right
                 for(int i = left; i<= right && head != null; i++){
                     res[top][i] = head.val;
                     head = head.next;
                 }
                 top++;
             }
-            else if(id == 1){
+            if(id == 1){ //top to down
                 for(int i = top; i<= down && head != null; i++){
                     res[i][right] = head.val;
                     head = head.next;
                 }
                 right--;
             }
-            else if(id == 2){
+            if(id == 2){ //right to left
                 for(int i = right; i >= left && head != null; i--){
                     res[down][i] = head.val;
                     head = head.next;
                 }
                 down--;
             }
-            else if(id == 3){
+            if(id == 3){ //down to top
                 for(int i = down; i >= top && head != null; i--){
                     res[i][left] = head.val;
                     head = head.next;
