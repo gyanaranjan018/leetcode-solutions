97ca6e3379719af1dc0ebc877fc4c9e4178efa15
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-09-26 19:36:22 +0530

    Time: 0 ms (100%), Space: 42.3 MB (62.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,51 +10,31 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode res = null, temp = null, ans = null;
+        ListNode head = new ListNode(-1);
+        ListNode res = head;
         while(list1 != null && list2 != null){
             if(list1.val < list2.val){
-                temp = new ListNode(list1.val);
+                res.next = new ListNode(list1.val);
                 list1 = list1.next;
             }
             else{
-                temp = new ListNode(list2.val);
+                res.next = new ListNode(list2.val);
                 list2 = list2.next;
             }
-            if(res == null){
-                res = temp;
-                ans = res;
-            }
-            else{
-                res.next = temp;
-                res = res.next;
-            }
+            res = res.next;
         }
-        if(list1 != null){
-            while(list1 != null){
-            if(res == null){
-                res = new ListNode(list1.val);
-                ans = res;
-            }
-            else{
-                res.next = new ListNode(list1.val);
-                res = res.next;
-            }
+
+        while(list1 != null){
+            res.next = new ListNode(list1.val);
+            res = res.next;
             list1= list1.next;
-            }
         }
-        if(list2 != null){
-            while(list2 != null){
-                if(res == null){
-                res = new ListNode(list2.val);
-                ans = res;
-            }
-            else{
-                res.next = new ListNode(list2.val);
-                res = res.next;
-            }
+
+        while(list2 != null){
+            res.next = new ListNode(list2.val);
+            res = res.next;
             list2 = list2.next;
-            }
         }
-        return ans;
+        return head.next;
     }
 }
