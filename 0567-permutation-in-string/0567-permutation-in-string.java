45006b0a49712efaad4063ed640e461435a84205
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-10-05 16:30:41 +0530

    Time: 50 ms (23.71%), Space: 45.3 MB (6.52%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int k = s1.length();
+        int i = 0, j = 0, n = s2.length();
+
+        int[] arr = new int[256];
+        for(char c : s1.toCharArray()){
+            arr[c-'a']++;
+        }
+
+        while(j < n){
+            arr[s2.charAt(j)-'a']--;
+
+            if(j-i+1 == k){
+                boolean check = true;
+                for(char c : s1.toCharArray()){
+                    if(arr[c-'a'] != 0){
+                        check = false;
+                        break;
+                    }
+                }
+                if(check){
+                    return true;
+                }
+                arr[s2.charAt(i)-'a']++;
+                i++;
+            }
+            j++;
+        }
+
+        return false;
+    }
+}
