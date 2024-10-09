c111f823440ce81c07a094fcddcbdbe258a7b923
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-10-10 01:51:50 +0530

    Time: 0 ms (100%), Space: 41.3 MB (79.22%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,5 +1,28 @@
 class Solution {
     public int minAddToMakeValid(String s) {
+
+        // Optimized approch
+        
+        int open = 0;
+        int close = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                open++;
+            }
+            else{
+                if(open <= 0){
+                    close++;
+                }
+                else{
+                    open--;
+                }
+            }
+        }
+
+        return open + close;
+
+        /*
         ArrayList<Character> list = new ArrayList<>();
         for(int i = 0; i < s.length(); i++){
             if(list.size() > 0 && list.get(list.size()-1) == '(' && s.charAt(i) == ')'){
@@ -10,5 +33,22 @@ class Solution {
             }
         }
         return list.size();
+
+        */
+
+
+
+        /*
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!st.isEmpty() && st.peek() == '(' && s.charAt(i) == ')'){
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        return st.size();
+        */
     }
 }
