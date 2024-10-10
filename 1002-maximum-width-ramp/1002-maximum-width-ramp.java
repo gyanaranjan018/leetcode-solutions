2af28ba70ccf4f58f287b82cfca2a98b0c56b64b
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2024-10-10 19:53:05 +0530

    Time: 3 ms (97.76%), Space: 54.6 MB (43.03%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+
+        // Optimal Approch
+        int n = nums.length, maxDepth = 0;
+        int[] maxToRight = new int[n];
+        maxToRight[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--){
+            maxToRight[i] = Math.max(maxToRight[i+1], nums[i]);
+        }
+
+        int i = 0, j = 0;
+        while(j < n){
+            if(nums[i] <= maxToRight[j]){
+                maxDepth = Math.max(maxDepth, j-i);
+                j++; 
+            }
+            else{
+                i++;
+                //j = i;
+            }
+        }
+
+        return maxDepth;
+
+        /*
+        //Better Approch
+        int n = nums.length, max_depth = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = n-1; j > i; j--){
+                if(nums[i] <= nums[j]){
+                    max_depth = Math.max(max_depth, j-i);
+                    break;
+                }
+            }
+        }
+        return max_depth;
+        */
+
+
+
+        /* Brute force - TLE, T.C  - O(n^2) 
+        int n = nums.length, max_depth = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(nums[i] <= nums[j]){
+                    max_depth = Math.max(max_depth, j-i);
+                }
+            }
+        }
+        return max_depth;
+        */
+    }
+}
