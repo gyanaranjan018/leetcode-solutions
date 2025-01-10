0081ff986876c5d535eef844c365eff9842cff62
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2025-01-11 01:39:11 +0530

    Time: 0 ms (100%), Space: 41 MB (53.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     // Top down approch(Recursion and memoization)
     public int rob(int[] nums) {
         int[] dp = new int[nums.length + 1];
@@ -16,5 +17,24 @@ class Solution {
         int steal = nums[i] + solve(nums, i+2, dp);
         int skip = solve(nums, i+1, dp);
         return dp[i] = Math.max(steal, skip);
+    }
+    */
+
+    //Buttom up approch
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        int[] dp = new int[n + 1];
+        //dp[i] = max stolen money till i house
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i<= n; i++){
+            int steal = nums[i-1] + dp[i - 2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(steal, skip);
+        }
+        return dp[n];
     }
 }
