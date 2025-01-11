4990c7ef40ed811dda6a00ffa3fe9ddf1159dcb5
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2025-01-12 01:42:12 +0530

    Time: 0 ms (100%), Space: 41.1 MB (58.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-
+    /*
+    // Top down approch
     public int solve(int[] nums, int i, int[] dp, int n){
         if(i > n){
             return 0;
@@ -28,5 +29,32 @@ class Solution {
         int take_1st_index_house = solve(nums, 1, dp, n-1);
 
         return Math.max(take_0th_index_house, take_1st_index_house);
+    }
+    */
+
+    //Buttom up approch
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        int[] dp = new int[n + 1];
+        //dp[i] = max stolen money till i house
+        //case 1 take first house
+        for(int i = 1; i<= n-1; i++){
+            int steal = nums[i-1] + ( i-2 >= 0 ? dp[i - 2] : 0);
+            int skip = dp[i-1];
+            dp[i] = Math.max(steal, skip);
+        }
+        int case1 = dp[n-1];
+        //case 2 skip first house
+        Arrays.fill(dp, 0);
+        for(int i = 2; i<= n; i++){
+            int steal = nums[i-1] + dp[i - 2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(steal, skip);
+        }
+        int case2 = dp[n];
+        return Math.max(case1, case2);
     }
 }
