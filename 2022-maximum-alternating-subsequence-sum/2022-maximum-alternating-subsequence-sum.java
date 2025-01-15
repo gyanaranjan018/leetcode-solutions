f8257430823081472a2b00a18888730dd426603c
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2025-01-16 00:35:36 +0530

    Time: 32 ms (59.91%), Space: 62.4 MB (68.34%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+    //Recursion + Memoization
     public long solve(int idx, boolean flag, int[] nums, int n, long[][] dp){
         if(idx >= n){
             return 0;
@@ -21,5 +23,20 @@ class Solution {
             Arrays.fill(row, -1);
         }
         return solve(0, true, nums, n, dp);
+    }
+    */
+
+    //Buttom Up Approch
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+        long[][] dp = new long[n+1][2];
+
+        for(int i = 1; i<= n; i++){
+            //even
+            dp[i][0] = Math.max(dp[i-1][1] - nums[i-1], dp[i-1][0]);
+            //odd
+            dp[i][1] = Math.max(dp[i-1][0] + nums[i-1], dp[i-1][1]);
+        }
+        return Math.max(dp[n][0], dp[n][1]);
     }
 }
