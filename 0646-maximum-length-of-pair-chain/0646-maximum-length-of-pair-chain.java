cc9b619353b7316fbf683d99aa85950504cefedb
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2025-01-20 00:46:16 +0530

    Time: 267 ms (5.01%), Space: 57.4 MB (5.2%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,5 +1,41 @@
 class Solution {
+
+    //Recursion + Memoization
+    public int solve(int[][] pairs, int idx, int prev, int[][] dp){
+        if(idx >= pairs.length){
+            return 0;
+        }
+
+        if(prev >= 0 && dp[idx][prev] != -1){
+            return dp[idx][prev];
+        }
+
+        int take = 0;
+        if(prev == -1 || pairs[idx][0] > pairs[prev][1]){
+            take = 1 + solve(pairs, idx+1, idx, dp);
+        }
+
+        int skip = solve(pairs, idx+1, prev, dp);
+
+        if(prev != -1){
+            dp[idx][prev] = Math.max(take, skip);
+        }
+        return Math.max(take, skip);
+    }
     public int findLongestChain(int[][] pairs) {
+        int n = pairs.length;
+        Arrays.sort(pairs, (p1, p2) -> p1[0] - p2[0]);
+        int[][] dp = new int[1001][1001];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        return solve(pairs, 0, -1, dp);
+    }
+
+
+    /*
+    public int findLongestChain(int[][] pairs) {
+        //Buttom up approch
         int n = pairs.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
@@ -16,4 +52,5 @@ class Solution {
 
         return maxLength;
     }
+    */
 }
