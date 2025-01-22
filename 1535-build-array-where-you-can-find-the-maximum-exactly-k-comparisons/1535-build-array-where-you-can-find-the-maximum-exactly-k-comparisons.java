b8eb79d1c51b3b652c4e37a72cec8d17a0214be8
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2025-01-23 00:59:51 +0530

    Time: 133 ms (53.95%), Space: 46.5 MB (35.53%) - LeetHub

---
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -0,0 +1,44 @@
+class Solution {
+    //Recursion + Memoization
+    private int N, M, K;
+    private final int MOD = 1000000007;
+    private int[][][] dp;
+    public int numOfArrays(int n, int m, int k) {
+        N = n;
+        M = m;
+        K = k;
+        dp = new int[51][51][101];
+
+        for(int i =0; i< 51; i++){
+            for(int j =0; j< 51; j++){
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return solve(0, 0, 0);
+    }
+
+    public int solve(int idx, int searchCost, int maxSoFar){
+        if(idx == N){
+            if(searchCost == K){
+                return 1;
+            }
+            return 0;
+        }
+
+        if(dp[idx][searchCost][maxSoFar] != -1){
+            return dp[idx][searchCost][maxSoFar];
+        }
+
+        int res = 0;
+        for(int i = 1; i <= M; i++){
+            if(i > maxSoFar){
+                res = (res + solve(idx+1, searchCost+1, i)) % MOD;
+            }
+            else{
+                res = (res + solve(idx+1, searchCost, maxSoFar)) % MOD;
+            }
+        }
+
+        return dp[idx][searchCost][maxSoFar] = res % MOD;
+    }
+}
