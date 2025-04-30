45670917106f6281c6300b547e89be88d5565bc6
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2025-04-30 15:20:18 +0530

    Time: 1 ms (98.76%), Space: 42.7 MB (88.02%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -58,9 +58,7 @@ class Solution {
         //Approch 5 (Basic observation of the input)
         int result = 0;
         for(int num : nums){
-            if((num > 9 && num < 100) || (num > 999 && num < 10000) || (num == 100000)){
-                result++;
-            }
+            if((num > 9 && num < 100) || (num > 999 && num < 10000) || (num == 100000)) result++;
         }
         return result;
     }
