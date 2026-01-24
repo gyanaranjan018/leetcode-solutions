ddd591db2801df8f71bcc2ae7320cce630fcf5f2
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2026-01-25 02:03:15 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.96%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,5 +1,5 @@
 class Solution {
-
+    /*
     // Memoization Approch
     // Initilize the dp array 
     int[] dp = new int[38];
@@ -27,5 +27,18 @@ class Solution {
     public int tribonacci(int n) {
         Arrays.fill(dp, -1);
         return solve(n);
+    }
+    */
+
+    // Tabulation approch
+    public int tribonacci(int n) {
+        int[] dp = new int[38];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i<= n; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
     }
 }
