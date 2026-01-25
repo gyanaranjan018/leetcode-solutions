40973e4a56836b40ae066896f2332f718fc750f5
Gyana Pradhan <42201004+gyanaranjan018@users.noreply.github.com>
2026-01-25 19:07:37 +0530

    Time: 0 ms (100%), Space: 43.8 MB (18.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     // DP on Grids solution 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid[0][0] == 1){
@@ -40,5 +41,29 @@ class Solution {
         }
 
         return dp[m-1][n-1];
+    }
+    */
+
+    // Optimized solution 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[j] = 0;
+                }
+                else if (j > 0){
+                    dp[j] = dp[j] + dp[j-1];
+                }
+            }
+        }
+        return dp[n-1];
     }
 }
